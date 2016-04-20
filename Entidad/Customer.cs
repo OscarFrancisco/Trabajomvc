534ca5ceef884ebd58a4ef1adde552af568e07b8
OscarFrancisco <theoscarf@hotmail.com>
2016-04-20 15:08:48 +0200

    commit & referencias

--- Entidad/Customer.cs
+++ Entidad/Customer.cs
@@ -8,6 +8,10 @@ namespace Dominio
 {
     public class Customer
     {
-        public int t { get; set; }
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Telefono { get; set; }
+        public string Correo { get; set; }
+        public string NombreUsuario { get; set; }
     }
 }
