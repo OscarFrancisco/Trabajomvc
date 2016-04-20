e7ec063e1c9569c471bce8b54fe1d04067dac59a
xperemiquel <x.peremiquel@gmail.com>
2016-04-20 17:24:32 +0200

    ultimocommitxavi

--- Infraestructura/Contexto.cs
+++ Infraestructura/Contexto.cs
@@ -1,13 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Dominio;
 
 namespace Infraestructura
 {
-    public class Contexto
+    public class Contexto : DbContext, IContexto
     {
+        public IDbSet<Customer> Usuario {get;set;}
 
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Configurations.Add(new Configuration.CustomerConfiguration());
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
---
+++ Infraestructura/IContexto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dominio;
+namespace Infraestructura
+{
+    public interface IContexto : IDisposable
+    {
+        IDbSet<Customer> Usuario { get; set; }
+    }
+}
