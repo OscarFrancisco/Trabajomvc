b62713185768c75bb5abcbbbe237e2db5ecae4c0
xperemiquel <x.peremiquel@gmail.com>
2016-04-20 15:25:37 +0200

    Eliminar IENTIDAD

--- Entidad/Customer.cs
+++ Entidad/Customer.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Dominio
 {
-    public class Customer : IEntidad
+    public class Customer
     {
         public int Id { get; set; }
         public string Nombre { get; set; }
