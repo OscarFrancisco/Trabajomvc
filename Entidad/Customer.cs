935fe2416ac6d78d68ff0d9cf1c5bac5dcf86f3b
xperemiquel <x.peremiquel@gmail.com>
2016-04-20 15:23:02 +0200

    Ientidad

--- Entidad/Customer.cs
+++ Entidad/Customer.cs
@@ -6,13 +6,12 @@ using System.Threading.Tasks;
 
 namespace Dominio
 {
-    public class Customer
+    public class Customer : IEntidad
     {
         public int Id { get; set; }
         public string Nombre { get; set; }
         public string Telefono { get; set; }
         public string Correo { get; set; }
         public string NombreUsuario { get; set; }
-
     }
 }
