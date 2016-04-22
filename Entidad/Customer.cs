33590cc327df45246997d8c55aa0d4d0ed487757
OscarFrancisco <theoscarf@hotmail.com>
2016-04-22 11:03:57 +0200

    Oscar:: Capa de Dominio 20160422

--- Entidad/Customer.cs
+++ Entidad/Customer.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,9 +10,17 @@ namespace Dominio
     public class Customer
     {
         public int Id { get; set; }
+        [Required]
+        [Display(Name = "Nombre:")]
         public string Nombre { get; set; }
+        [Required]
+        [Display(Name = "Telefono:")]
         public string Telefono { get; set; }
+        [Required]
+        [Display(Name = "Correo:")]
         public string Correo { get; set; }
-        public string NombreUsuario { get; set; }
+        [Required]
+        [Display(Name = "Nombre Usuario:")]
+        public string NombreUsuario { get; set; } 
     }
 }
