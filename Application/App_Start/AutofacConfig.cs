a3865e4e8244523ba478ba9b65af608a2a72e1fb
xperemiquel <x.peremiquel@gmail.com>
2016-04-25 15:19:08 +0200

    AutoFac

---
+++ Application/App_Start/AutofacConfig.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Autofac;
+using System.Web.Mvc;
+using Autofac.Integration.Mvc;
+using Infraestructura;
+using Dominio;
+using Servicio;
+using Application.Controllers;
+
+
+namespace Application.App_Start
+{
+    public class AutofacConfig
+    {
+
+        public  void Configurar()
+        {
+            var builder = new ContainerBuilder();
+            var container = builder.Build();
+
+            builder.RegisterControllers(typeof(MvcApplication).Assembly);
+            builder.RegisterModelBinders(typeof(MvcApplication).Assembly);
+            builder.RegisterModule<AutofacWebTypesModule>();
+
+            builder.RegisterType<CustomerController>().As<Controller>().InstancePerHttpRequest();
+            builder.RegisterType<CustomerServicio>().As<ICustomerServicio<CustomerServicio>>().InstancePerHttpRequest();
+            builder.RegisterType<AppContext>().As<Infraestructura.IRepositorio>().InstancePerHttpRequest();
+
+            DependencyResolver.SetResolver(new AutofacDependencyResolver(container));        
+        }
+    }
+}
--- Application/Global.asax.cs
+++ Application/Global.asax.cs
@@ -6,6 +6,8 @@ using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using Application.Controllers;
+using Application.App_Start;
 
 namespace Application
 {
@@ -17,12 +19,14 @@ namespace Application
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
-
             WebApiConfig.Register(GlobalConfiguration.Configuration);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
             AuthConfig.RegisterAuth();
+            var Autofac = new AutofacConfig();
+            Autofac.Configurar();
+
         }
     }
 }
